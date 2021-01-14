9a15bd13192841ab7ee9f01e9e3ae86d7aefaca9
David N <nguyen.david94579@gmail.com>
2021-01-13 16:01:27 -0800

    Added users get id route

--- flask_backend/sample_backend.py
+++ flask_backend/sample_backend.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -42,4 +42,21 @@ users = {
 
 @app.route('/users')
 def get_users():
+    search_username = request.args.get('name')  # accessing the value of parameter 'name'
+    if search_username:
+        subdict = {'users_list': []}
+        for user in users['users_list']:
+            if user['name'] == search_username:
+                subdict['users_list'].append(user)
+        return subdict
+    return users
+
+
+@app.route('/users/<id>')
+def get_user(id):
+    if id:
+        for user in users['users_list']:
+            if user['id'] == id:
+                return user
+        return {}
     return users
